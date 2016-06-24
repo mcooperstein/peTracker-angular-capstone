f0a4a83ee5afc4835e354e451d3419e926aca83d
Marc Cooperstein <mcooperstein@gmail.com>
2016-06-24 16:45:46 -0700

    cleaned up some code to make more maintainable and have routes linked to pentane variable
    cleaned up some code to make more maintainable and have routes linked
    to pentane variable

--- app/js/app.js
+++ app/js/app.js
@@ -13,7 +13,7 @@ app.value('petprofile', [
             vet: "Annual checkup at Vet on 5/30/16, had 2 teeth pulled."
         },
         appointments: ["Vet checkup on 6/15/16", "Grooming Appointment on 7/1/16"],
-        events: ["Riley peed 8am", "Riley fed 9am"]
+        actions: ["Riley peed 8am", "Riley fed 9am"]
     },
     //Moe
     {
@@ -25,7 +25,7 @@ app.value('petprofile', [
             checkup: "1 year checkup at Vet on 5/30/16, very healthy"
         },
         appointments: ["Vet checkup on 6/25/16", "Grooming Appointment on 6/26/16", "Grooming Appointment on 8/3/16"],
-        events: ["Moe peed 7am", "Moe ran 10am"]
+        actions: ["Moe peed 7am", "Moe ran 10am"]
 
     }
 ]);
@@ -59,15 +59,23 @@ app.config(['$locationProvider', '$routeProvider',
                     controller: 'dailylogController',
                     resolve: {
                         petname: function (petprofile, $route, $location) {
-                            var petname = $route.current.params.petname;
+                                var petname = $route.current.params.petname;
 
-                            //                            angular.forEach(petprofile, function (value, key) {
-                            //                                if (value.petname === petname) {
-                            //                                    return petname;
-                            //                                }
-                            //                            });
-                            return petname;
-                        }
+                                //                            angular.forEach(petprofile, function (value, key) {
+                                //                                if (value.petname === petname) {
+                                //                                    return petname;
+                                //                                }
+                                //                            });
+                                return petname;
+                            }
+                            /*actions: function (petprofile, $route, $location) {
+                                var actions = $route.current.params.events;
+                                if (value.petname === "Riley") {
+                                    return petprofile[0].actions
+                                } else {
+                                    return petprofile[1].actions
+                                }
+                            }*/
                     }
                 })
                 .when('/medical/:petname', {
@@ -128,28 +136,6 @@ app.config(['$locationProvider', '$routeProvider',
         $rootScope.$on('$routeChangeSuccess', function () {
             $rootScope.isLoading = false;
         }, 2000);
-        /*$rootScope.$on('addEvents', function () {
-            if ($rootScope.addText.length >= 1) {
-                $rootScope.events.push({
-                    text: $rootScope.addText
-                });
-            }
-            $rootScope.addText = "";
-        });
-        $rootScope.$on('checkEvents', function (event) {
-            if ($rootScope.event.done == true) {
-                delete $rootScope.event.done;
-            }
-        });
-        $rootScope.$on('clearCompleted', function () {
-            var doneEvents = $rootScope.events;
-            $rootScope.events = [];
-            angular.forEach(doneEvents, function (event) {
-                if (!event.done) {
-                    $rootScope.events.push(event);
-                }
-            });
-        });*/
     });
 app.controller('MainController', ['$scope', 'petprofile', function ($scope, petprofile) {
     $scope.petprofile = petprofile;
@@ -192,29 +178,6 @@ app.controller('MainController', ['$scope', 'petprofile', function ($scope, petp
 
 }]);
 
-/*app.controller('moeController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-    $scope.moedaylog = [
-        {
-            pee: 'Went pee  at 8am',
-            fed: 'Fed breakfast 9am',
-            run: 'Taken for run 10am',
-            poo: 'Pooped in backyard at 11am'
-        }
-    ];
-    $scope.moemedical = [
-        {
-            neutered: 'Moe was neutered at SPCA on 1/25/16.',
-            rabies: 'Got all shots/vaccinations on 3/3/16',
-            checkup: '1 year checkup at Vet on 5/30/16'
-        }
-    ];
-    $scope.moeappointments = [
-        {
-            vet: 'Vet checkup on 7/7/16',
-            groomer: 'Grooming apt on 7/15/16'
-        }
-    ];
-}]);*/
 
 app.controller('petprofileController', ['$scope', 'petprofile', 'petname', function ($scope, petprofile, petname) {
     $scope.petprofile = petprofile;
@@ -225,93 +188,268 @@ app.controller('petprofileController', ['$scope', 'petprofile', 'petname', funct
 app.controller('dailylogController', ['$scope', 'petprofile', 'petname', function ($scope, petprofile, petname) {
     $scope.petprofile = petprofile;
     $scope.petname = petname;
+    /*$scope.actions = actions;*/
     console.log(petname);
-    /*$scope.events = [];
-
-        $scope.moes = [
-            {
-                text: "8am put outside, but didn't do anything"
-            }
-        ];
-
-        $scope.pees = [
-            {
-                text: "8am went pee",
-                text: "Fed at 730am"
-            }
-        ];
-*/
+    $scope.events = [];
     $scope.today = new Date();
-    /*$scope.events = [
-        {
-            text: 'Went pee at 8am'
-        },
-        {
-            text: 'Fed Breakfast at 9am'
-        },
-        {
-            text: 'Taken for run at 10am'
-        },
-        {
-            text: 'Pooped in backyard at 11am'
+    $scope.actions = ["Moe pooped 8am", "Moe fed 9am", "Riley fed 7am", "Riley pee 730am", "actions[4]", "actions[5]"];
+
+    $scope.displayActions = function () {
+        if ($scope.petname == "Moe") {
+            //This shows actions[0] and actions[1], but leaves out actions[4] and actions[5]
+            $scope.actions.splice(2, 2)
+        } else {
+            //This shows entire actions array
+            //$scope.actions.splice(0, 2, 'Riley fed 7am', 'Riley poo 8am')
+            //This shows nothing
+            //$scope.actions.splice(0, 4)
+            //This shows 3 items I 'pop' into array
+            $scope.actions.splice(0, 4, 'Riley fed 7am', 'Riley poo 730am', 'Riley run 9am')
+                //This shows all 4 items I pop into array plus actions[4] and actions[5]
+                //$scope.actions.splice(0, 4, 'Riley fed 7am', 'Riley poo 730am', 'Riley run 9am', "Riley pee and poo 12pm")
+
         }
-    ];*/
-    /*
-        $scope.addEvents = function () {
-            if ($scope.addText.length >= 1) {
-                $scope.events.push({
-                    text: $scope.addText
-                });
-            }
-            $scope.addText = "";
-        };
+        return $scope.actions
+    };
 
-        $scope.checkEvents = function (event) {
-            if ($scope.event.done == true) {
-                delete $scope.event.done;
+    /*$scope.actions = [
+        {
+            moe: "Moe pooped 8am"
+        },
+        {
+            moe: "Moe fed 9am"
+        },
+        {
+            riley: "Riley fed 7am"
+        },
+        {
+            riley: "Riley peed 730am"
+        }
+    ];
+
+    $scope.displayActions = function () {
+        if ($scope.petname == "Moe") {
+            $scope.actions = $scope.actions.splice(2, 2);
+        } else {
+            $scope.actions = $scope.actions.splice(0, 2);
+        }
+        return $scope.actions
+    };*/
+
+    /*$scope.actions = {
+        moe: "Moe pooped 8am",
+        moe: "Moe fed 9am",
+        riley: "Riley fed 7am",
+        riley: "Riley peed 730am"
+    };*/
+    /*$scope.actions = {
+        Moe: ["Moe pooped 8am", "Moe fed 9am"],
+        Riley: ["Riley fed 7am", "Riley peed 730am"]
+    };
+
+    var displayActions = function (actionsArray, inputPetname) {
+
+        var petActionKey = 0;
+        var petAction = [];
+        for (var actionPetName in actionsArray) {
+            if (actionPetName === inputPetname) {
+                petAction[petActionKey] = actionsArray[actionPetName];
+                petActionKey++;
             }
         };
+        return petAction;
+    };
+    console.log(displayActions($scope.actions, "Riley"));*/
 
-        $scope.clearCompleted = function () {
-            var doneEvents = $scope.events;
-            $scope.events = [];
-            angular.forEach(doneEvents, function (event) {
-                if (!event.done) {
-                    $scope.events.push(event);
-                }
+    //$scope.actions = [];
+
+    /*var riley = ["Riley fed 7am", "Riley pee 730am", "Riley run at 9am"];
+    var moe = ['Moe pooped 8am', 'Moe fed 9am'];
+
+    $scope.actions = function () {
+        if ($scope.petname == "Riley") {
+            $scope.actions = riley;
+        } else {
+            $scope.actions = moe;
+        }
+        return $scope.actions
+    };
+
+    console.log($scope.actions);*/
+
+
+
+    /*$scope.addActions = function () {
+        if ($scope.petname == "Riley") {
+            $scope.actions.push({
+                text: "Riley went pee 8am",
+                text: "Riley went poo 9am"
             });
-        };*/
-}]);
+        } else if ($scope.petname == "Moe") {
+            $scope.actions.push({
+                text: "Moe fed at 8am",
+                text: "Moe pee and poo 10am"
+            });
+        } else {
+            $scope.actions.push({
+                text: "error loading info"
+            });
+        }
+    };*/
+
+    /*$scope.actions = function () {
+        if ($scope.petname == "Riley") {
+            return "Riley went pee"
+        } else if ($scope.petname == "Moe") {
+            return "Moe went poo"
+        } else {
+            return "error"
+        }
+    };*/
+
+    /*$scope.addActions = function () {
+        if ($scope.petname == "Riley") {
+            $scope.actions.push({
+                text: $scope.text
+            });
+            $scope.text = "Riley went pee";
+        }
+        //$scope.addText = "Riley went pee";
+        else if ($scope.petname == "Moe") {
+            $scope.actions.push({
+                text: $scope.text
+            });
+            $scope.text = "Moe went poo"
+        } else {
+            $scope.actions.push({
+                text: $scope.text
+            });
+            $scope.text = "Error"
+        }
+    };*/
+
+    $scope.addEvents = function () {
+        if ($scope.addText.length >= 1) {
+            $scope.events.push({
+                text: $scope.addText
+            });
+        }
+        $scope.addText = "";
+    };
+
+
+    $scope.checkEvents = function (event) {
+        if ($scope.event.done == true) {
+            delete $scope.event.done;
+        }
+    };
+
+    $scope.clearCompleted = function () {
+        var doneEvents = $scope.events;
+        $scope.events = [];
+        angular.forEach(doneEvents, function (event) {
+            if (!event.done) {
+                $scope.events.push(event);
+            }
+        });
+    };
+            }]);
 
 app.controller('medicalController', ['$scope', 'petprofile', 'petname', function ($scope, petprofile, petname) {
     $scope.petprofile = petprofile;
     $scope.petname = petname;
     //console.log(petname);
+    $scope.events = [];
+    $scope.addEvents = function () {
+        if ($scope.addText.length >= 1) {
+            $scope.events.push({
+                text: $scope.addText
+            });
+        }
+        $scope.addText = "";
+    };
+
+    $scope.checkEvents = function (event) {
+        if ($scope.event.done == true) {
+            delete $scope.event.done;
+        }
+    };
+
+    $scope.clearCompleted = function () {
+        var doneEvents = $scope.events;
+        $scope.events = [];
+        angular.forEach(doneEvents, function (event) {
+            if (!event.done) {
+                $scope.events.push(event);
+            }
+        });
+    };
+
+    $scope.medicals = ["Moe was neutered at SPCA on 1/15/16", "Moe got rabies/DA2PP shots (3-year vaccines) 3/3/16", "1 year checkup at Vet on 5/30/16, very healthy", "Riley pee 730am", "medicals[4]", "medicals[5]"];
+
+    $scope.displayMedicals = function () {
+        if ($scope.petname == "Moe") {
+            //This shows actions[0] and actions[1], but leaves out actions[4] and actions[5]
+            $scope.medicals.splice(3, 3)
+        } else {
+
+            //This shows 4 items I 'pop' into array
+            $scope.medicals.splice(0, 5, 'Riley was neutered at SPCA on 2/28/04', 'Got last rabies/DA2PP vaccinations 9/3/14 (3-year vaccine)', 'Annual checkup at Vet on 4/25/15, vet found 3 fleas.', 'Riley got 2 teeth pulled at annual checkup 6/21/16')
+                //This shows all 5 items I pop into array plus medicals[4] and medicals[5]
+                //$scope.medicals.splice(0, 4, 'Riley fed 7am', 'Riley poo 730am', 'Riley run 9am', "Riley pee and poo 12pm", "Riley fed 6pm")
+
+        }
+        return $scope.medicals
+    };
+
 }]);
 
 app.controller('appointmentsController', ['$scope', 'petprofile', 'petname', function ($scope, petprofile, petname) {
     $scope.petprofile = petprofile;
     $scope.petname = petname;
     //console.log(petname);
+    /*$scope.appointments = [{
+        text: "Moe"
+    }, {
+        text: "Riley"
+    }, {
+        text: "Poop"
+    }];*/
+    //$scope.events = [];
 
+    $scope.appointments = [{
+        text: "Moe grooming apt 6/3/16"
+    }, {
+        text: "Moe grooming apt 7/15/16"
+    }, {
+        text: "Moe vet appointment 9/16/16"
+    }, {
+        text: "appointments[3]"
+    }, {
+        text: "appointments[4]"
+    }, {
+        text: "appointments[5]"
+    }];
+
+    $scope.displayAppointments = function () {
+        if ($scope.petname == "Moe") {
+            $scope.appointments.splice(3, 3)
+        } else {
+            $scope.appointments.splice(0, 5, "Riley oral surgeon appointment 9/15/16")
+        }
+        return $scope.appointments
+    };
+    //console.log($scope.appointments[i]);
 
     $scope.addAppointments = function () {
         if ($scope.addText.length >= 1) {
-            $scope.rileyappointments.push({
+            $scope.appointments.push({
                 text: $scope.addText
             });
         }
         $scope.addText = "";
     };
 
-    $scope.addMoeAppointments = function () {
-        if ($scope.addText.length >= 1) {
-            $scope.moeappointments.push({
-                text: $scope.addText
-            });
-        }
-        $scope.addText = "";
-    };
 
     $scope.checkAppointments = function (appointment) {
         if ($scope.appointment.done == true) {
@@ -320,22 +458,22 @@ app.controller('appointmentsController', ['$scope', 'petprofile', 'petname', fun
     };
 
     $scope.clearCompleted = function () {
-        var doneAppointments = $scope.rileyappointments;
-        $scope.rileyappointments = [];
+        var doneAppointments = $scope.appointments;
+        $scope.appointments = [];
+        /*var doneEvents = $scope.events;
+        $scope.events = [];*/
         angular.forEach(doneAppointments, function (appointment) {
             if (!appointment.done) {
-                $scope.rileyappointments.push(appointment);
+                $scope.appointments.push(appointment);
             }
         });
+        /*angular.forEach(doneEvents, function (event) {
+            if (!event.done) {
+                $scope.events.push(event);
+            }
+        });*/
     };
 
-    $scope.clearMoeCompleted = function () {
-        var doneAppointments = $scope.moeappointments;
-        $scope.moeappointments = [];
-        angular.forEach(doneAppointments, function (appointment) {
-            if (!appointment.done) {
-                $scope.moeappointments.push(appointment);
-            }
-        });
-    };
+
+
 }]);
