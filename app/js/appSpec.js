b184acac2b940976f602d7b50c094ac73c4646c5
Marc Cooperstein <mcooperstein@gmail.com>
2016-07-08 15:53:35 -0700

    added karma/jasmine test
    added karma/jasmine test

--- app/js/appSpec.js
+++ app/js/appSpec.js
@@ -7,23 +7,24 @@
     $scope = null;
 
     beforeEach(function () {
-        angular.module('myApp');
+        angular.module('myApp')
+            .controller('petprofileController', ['$scope', 'petprofile', 'petname', function ($scope, petprofile, petname) {}]);
     });
     //beforeEach(angular.module('myApp'));
 
 
-    beforeEach(inject(function ($controller, $rootScope) {
-        $//controller = controller;
+    beforeEach(inject(['$controller', '$rootScope', function ($controller, $rootScope) {
+        //controller = controller;
         //console.log($rootScope);
         $scope = $rootScope.$new();
-        controller('petprofileController', {
+        $controller('petprofileController', {
             $scope: $scope
         });
-    }));
+    }]));
 
-    it('initial petname value should equal current petname', function (petprofile) {
+    it('initial petname value should equal current petname', function ($scope, petprofile) {
         //console.log($scope);
-        expect($scope.petprofile).toEqual(petprofile);
+        expect(petprofile).toEqual($scope.petprofile);
     });
 
 });*/
@@ -31,22 +32,22 @@
 //this test checks to make sure html templates load as expected
 
 describe('Routes', function () {
-
-    beforeEach(function () {
+    beforeEach(module('myApp'));
+    /*beforeEach(function () {
         angular.module('myApp');
-    });
+    });*/
 
     //when route is '/partials/homepage', does homepage load properly ?
     it('should load the homepage', function () {
         inject(["$route", "$rootScope", "$location", "$httpBackend", function ($route, $rootScope, $location, $httpBackend) {
-            /*var route = $route.routes['/homepage'];
+            var route = $route.routes['/homepage'];
             $httpBackend.whenGET(route.templateUrl).respond('...');
 
             $rootScope.$apply(function () {
                 $location.path(route.originalPath);
             });
 
-            expect($route.current.templateUrl).toBe('./partials/homepage.html');*/
+            expect($route.current.templateUrl).toBe('./partials/homepage.html');
         }]);
     });
 });
--- app/js/protractortest.js
+++ app/js/protractortest.js
@@ -2,5 +2,6 @@ describe('homepage', function () {
     it('should visit peTracker and find the homepage', function () {
         browser.get('http://mcooperstein.github.io/peTracker-angular-capstone/app/index.html');
         expect(element(by.css('h2')).getInnerHtml()).toMatch(/Select Your Pet's Profile Below:/);
+        expect(element(by.css('h1')).getInnerHtml()).toMatch(/Welcome to PeTracker - The Best way to Track your Pet's Needs/);
     });
 });
